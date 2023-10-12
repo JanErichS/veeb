112f82af88fe1ea695aaafc3c5bd323ae5adce30
Jan-Erich Sigur <JanErichS@users.noreply.github.com>
2023-10-12 17:28:47 +0300

    Tund 6 tehtud; nimed poolik

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const express = require('express'); 
+const dateTime = require('./dateTime_ET');
+const fs = require('fs');
 const app = express(); 
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
 
 app.get('/',(req,res)=>{
     res.render('index');
 });
 
-app.get('/test',(req,res)=>{
-    res.send('test test 1 2 3');
+app.get('/timenow',(req,res)=>{
+    const dateNow = dateTime.dateETformatted();
+    const timeNow = dateTime.timeETformatted();
+    res.render('timenow', {dateN: dateNow, timeN: timeNow});
+});
+
+app.get('/folk',(req,res)=>{
+    let folkWisdom = [];
+    fs.readFile('public/txtfiles/vanasona.txt', 'utf8', (err, data)=>{
+        if(err){
+            throw err;
+        }
+        else{
+            folkWisdom = data.split(';');
+            res.render('justlist', {h1: 'Vanasõnad', wisdom: folkWisdom});
+        }
+    });
+});
+
+app.get('/guests', (req,res)=>{
+    let guestList = [];
+    fs.readFile('public/txtfiles/namelog.txt', 'utf8', (err, data)=>{
+        if (err){
+            throw err;
+        }
+        else{
+            guestList = data.split(';');
+            console.log(guestList);
+            res.render('namelist', {guest: guestList});
+        }
+    });
 });
 
 app.listen(5133);
@@ -20,3 +52,7 @@ app.listen(5133);
  // res.send -> saadab lihtsalt koodi enda
  // res. render -> võtab html koodi
  // ejs mooduli sai -> npm install ejs; index peab ümber nimetama .ejs extensioninga
+ // .use -> nõuab vahevara/ middleware ;static -> koht ei muutu, sulgudes dir
+ // require arvab, et kõik failid .js ext ehk ei pea seda kirja panema
+ // res.render('timenow', {dateN: dateNow, timeN: timeNow});; --> {objekti nimetus: objekti väärtus, uue objekt inimetus: uue objekt iväärtus}
+ // <%= ejs muutuja nimi %> ; ejs moodulite kasutamine html-is

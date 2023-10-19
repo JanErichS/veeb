89c1dbcea78604c05dedaf035a120ff580504b35
Jan-Erich Sigur <JanErichS@users.noreply.github.com>
2023-10-19 17:28:10 +0300

    Seitsmenda tunni töö; poolik

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 const express = require('express'); 
-const dateTime = require('./dateTime_ET');
 const fs = require('fs');
 const app = express(); 
+const mysql = require('mysql2');
+const bodyparser = require('body-parser');
+
+// isetehtud moodulid
+const dateTime = require('./dateTime_ET');
+const dbInfo = require('../../vp23config')
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyparser.urlencoded({extended: false}));
 
-app.get('/',(req,res)=>{
+// andmebaasiga ühendus
+const conn = mysql.createConnection({
+    host: dbInfo.configData.host,
+    user: dbInfo.configData.user,
+    password: dbInfo.configData.password,
+    database: dbInfo.configData.database
+});
+
+// avaleht
+app.get('/', (req, res)=>{
     res.render('index');
 });
 
-app.get('/timenow',(req,res)=>{
+// kellaaeg
+app.get('/timenow', (req, res)=>{
     const dateNow = dateTime.dateETformatted();
     const timeNow = dateTime.timeETformatted();
     res.render('timenow', {dateN: dateNow, timeN: timeNow});
 });
 
-app.get('/folk',(req,res)=>{
+// vanasõnad
+app.get('/folk', (req, res)=>{
     let folkWisdom = [];
     fs.readFile('public/txtfiles/vanasona.txt', 'utf8', (err, data)=>{
         if(err){
@@ -29,7 +46,8 @@ app.get('/folk',(req,res)=>{
     });
 });
 
-app.get('/guests', (req,res)=>{
+// külastajad
+app.get('/guests', (req, res)=>{
     let guestList = [];
     fs.readFile('public/txtfiles/namelog.txt', 'utf8', (err, data)=>{
         if (err){
@@ -56,22 +74,82 @@ app.get('/guests', (req,res)=>{
             /*console.log(guestList)
             console.log(person)*/
             nameDate = dateTime.ENtoET(nameDate);
-            res.render('namelist', {guest: guestList, person: person, nameDate: nameDate}); // guestList -> kõik nimekirja elemendid; person -> ainult nimi; date -> kuupäev
+            res.render('namelist', {guest: guestList, person: person, nameDate: nameDate}); // guestList -> kõik nimekirja elemendid; person -> ainult nimi; nameDate -> kuupäev
         }
     });
 });
 
+// filmide avaleht
+app.get('/movies', (req, res)=>{
+    res.render('filmindex');
+});
+
+// filmide nimekiri
+app.get('/movies/filmiloend', (req, res)=>{
+    let sql = 'SELECT id, title, production_year, duration FROM movie';
+    let sqlResult = [];
+    conn.query(sql, (err, result)=>{
+        if (err){
+            res.render('filmlist', {filmList: result});
+            throw err;
+        }
+        else{
+            res.render('filmlist', {filmList: result});
+        }
+    });
+});
+
+// filmitegelase lisamine
+app.get('/movies/addfilmperson', (req, res)=>{
+    res.render('addfilmperson');
+});
+
+app.get('/movies/singlemovie', (req, res)=>{
+    let sql = 'SELECT count(id) FROM movie';
+    conn.query(sql, (err, result)=>{
+        if (err){
+            throw err;
+        }
+        else{
+            console.log(result)
+            res.render('singlemovie', {movieNum: result})
+        }
+    });
+});
+
+// funktsioonid
 function openArr(bigArr){
     bigArr.pop(); // eemaldab viimase tühja elemendi
     smolArr = bigArr.toString(); // muudab array tagasi stringiks
     bigArr = smolArr.split(','); // loob stringist uue array, ilma komadetta --> väga ebaefektiivne.
     return bigArr
-}
+};
+
+// formsi jaoks post rooting
+app.post('/movies/addfilmperson', (req, res)=>{
+    let notice = '';
+    let sql = 'INSERT INTO person (first_name, last_name, birth_date) VALUES(?,?,?)';
+    conn.query(sql, [req.body.firstNameInput, req.body.lastNameInput, req.body.dateOfBirth], (err, result)=>{
+        if (err){
+            notice = 'Andmete salvestamine ebaõnnestus (bad ending)';
+            res.render('addfilmperson', {notice: notice});
+            throw err;
+        }
+        else{
+            notice = req.body.firstNameInput + ' ' + req.body.lastNameInput + ' salvestamine õnnestus';
+            res.render('addfilmperson', {notice: notice})
+        }
+    });
+});
+
+app.post('/movies/singlemovie', (req, res)=>{
+
+});
 
 app.listen(5133);
 
 // comments
-// Express.js framework
+ // Express.js framework
  // võib teha ka res.download('index.js'); -> laeb veebilehe alla; res -> response/vaste, req -> request/päring
  // app -> standard jälle
  // res.send -> saadab lihtsalt koodi enda
@@ -81,3 +159,13 @@ app.listen(5133);
  // require arvab, et kõik failid .js ext ehk ei pea seda kirja panema
  // res.render('timenow', {dateN: dateNow, timeN: timeNow});; --> {objekti nimetus: objekti väärtus, uue objekt inimetus: uue objekt iväärtus}
  // <%= ejs muutuja nimi %> ; ejs moodulite kasutamine html-is
+
+ //     conn.query(sql, (err, result, fields) --> fields annab tehnilise mumbo-jumbo
+ // formsidel peab olema eraldi root POSTile js failis
+ // app.use(bodyparser.urlencoded({extended: false})); võtab päringu(req) ning decodeerib selle; extended false --> andmed on ainult tekstiinfo
+ // app.set('view engine', 'ejs'); --> defineerib app engine  
+ // app.use(express.static('public')); ---> lubab public kataloogi kasutada
+ // conn.end() -> lõpetab ühenduse db.ga
+ // INSERT INTO person (first_name, last_name, birth_date) VALUES(?,?,?) --> küsimärgid ei lase teha injection type attacki nii kergelt? 
+ // conn.query(sql, [req.body.firtNameInput, req.body.lastNameInput, req.body.dateOfBirth]) paneb küsimärkide asemele andmed?
+ // addfilmperson --> locals.notice kui väärtus puudub, näitab tühjust
